cdcfc34d1c72596fd22d06153d526f6ee42af035
VedantPShah <94154986+VedantPShah@users.noreply.github.com>
2021-12-15 18:41:34 -0600

    Add files via upload

---
+++ WebApplication2/Delete.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebApplication2
+{
+    public partial class Delete : System.Web.UI.Page
+    {
+        SqlConnection conn;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+                conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+                conn.Open();
+                string value = Request.QueryString["room"];
+                //string message = Request.QueryString["message"];
+                string sess = Session["ses"].ToString(); // AND message='" + message + "'
+                string Query = "DELETE FROM " + sess + " WHERE room='" + value + "'";
+                SqlCommand cmd = new SqlCommand(Query, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            
+                Response.Redirect("AdminDemo1.aspx");
+        }
+    }
+}
